318c70b5c21847888538088131410bf9d5c9b1ee
lucy3 <luxisufeili@gmail.com>
2016-10-24 18:48:37 -0700

    I'm guessing the game is a class

---
+++ dominoes.py
@@ -0,0 +1,25 @@
+
+# Game skeleton
+# Feel free to rip apart because idk what I'm doing
+
+class Dominoes(object):
+	def __init__(self): 
+		pass
+
+	def startState(self):
+		pass
+
+	def isEnd(self, state):
+		'''
+		The game ends when everyone passes once
+		Also when one person has no dominoes left? 
+		'''
+		pass
+
+	def actions(self, state):
+		pass
+
+	def states(self):
+		pass
+
+		
